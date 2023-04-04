1b46762cc69da4a064a9810b898ab5302b08d33f
projet-echec1 <129861193+projet-echec1@users.noreply.github.com>
2023-04-04 12:58:42 +0000

    D

--- chess.py
+++ chess.py
@@ -1,3 +1,46 @@
+class ChessGame:
+    
+    def __init__(self):
+
+        class Piece:
+            def __init__(self,couleur,symbole):
+            self.symbole=symbole
+            self.couleur=couleur
+ 
+
+        self.echiquier = [ [Piece('noir', 'R'), Piece('noir', 'N'), Piece('noir', 'B'), Piece('noir', 'Q'), Piece('noir', 'K'), Piece('noir', 'B'), Piece('noir', 'N'), Piece('noir', 'R')]
+                      , [Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P')],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None], 
+                      [Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P')],
+                      [Piece('blanc', 'R'), Piece('blanc', 'N'), Piece('blanc', 'B'), Piece('blanc', 'Q'), Piece('blanc', 'K'), Piece('blanc', 'B'), Piece('blanc', 'N'), Piece('blanc', 'R')] ] 
+
+        self.tour = 'blanc'
+
+
+
+
+
+    def obtenir_piece(self, position):
+
+        ligne, colonne = position 
+
+        return self.echiquier[ligne][colonne]
+    
+
+    class Piece:
+        def __init__(self,couleur,symbole):
+            self.symbole=symbole
+        self.couleur=couleur    
+    
+        def case_roi(self):
+            for i in range(8):
+                for j in range(8):
+                    piece=self.obtenir_piece(i,j)
+                    if piece.couleur==couleur and 
+
 
 #Mouvements
 
@@ -18,7 +61,7 @@
         
             cavalier=[[self.choixpiece[0]+2,self.choixpiece[0]+1],[self.choixpiece[0]+2,self.choixpiece[0]-1],[self.choixpiece[0]-2,self.choixpiece[0]-1],[self.choixpiece[0]-2,self.choixpiece[0]+1],[self.choixpiece[0]+1,self.choixpiece[0]+2],[self.choixpiece[0]+1,self.choixpiece[0]-2],[self.choixpiece[0]-1,self.choixpiece[0]-2],[self.choixpiece[0]-1,self.choixpiece[0]+2]]]
     
-   """ 
+ 
    def pion(self):
         if self.turn==1 or self.turn==2: 
             pion=[[self.choixpiece[0]+1,self.choixpiece[0]+1],[self.choixpiece[0]+2,self.choixpiece[0]+2]]
@@ -28,47 +71,12 @@
     
     def tour(self):
         tour
-        for i in range 1,8
+        for i in range (1,8):
         
-       """ 
-class Piece:
-    def __init__(self,couleur,symbole):
-        self.symbole=symbole
-        self.couleur=couleur
-            
+
+           
 
 
 
 
 
-class ChessGame: 
-
-    def __init__(self):  
-
-        self.echiquier = [ [Piece('noir', 'R'), Piece('noir', 'N'), Piece('noir', 'B'), Piece('noir', 'Q'), Piece('noir', 'K'), Piece('noir', 'B'), Piece('noir', 'N'), Piece('noir', 'R')]
-                      , [Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P'), Piece('noir', 'P')],
-                      [None, None, None, None, None, None, None, None],
-                      [None, None, None, None, None, None, None, None],
-                      [None, None, None, None, None, None, None, None],
-                      [None, None, None, None, None, None, None, None], 
-                      [Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P'), Piece('blanc', 'P')],
-                      [Piece('blanc', 'R'), Piece('blanc', 'N'), Piece('blanc', 'B'), Piece('blanc', 'Q'), Piece('blanc', 'K'), Piece('blanc', 'B'), Piece('blanc', 'N'), Piece('blanc', 'R')] ]  
-
-        self.tour = 'blanc' 
-
- 
-
- 
-
-    def obtenir_piece(self, position): 
-
-        ligne, colonne = position  
-
-        return self.echiquier[ligne][colonne] 
-    
-    
-    def case_roi(self):
-        for i in range(8):
-            for j in range(8):
-                piece=self.obtenir_piece(i,j)
-                if piece.couleur==couleur and 
